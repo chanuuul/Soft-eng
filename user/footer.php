a0bc9dda2e31f99319c62421bebc472a5c2cf7a8
chanuuul <christianrobles0024@gmail.com>
2022-03-15 22:27:18 +0800

    first commit

---
+++ user/footer.php
@@ -0,0 +1,12 @@
+<style type="text/css">
+<!--
+.style1 {
+	color: #000000;
+	font-weight: bold;
+}
+-->
+</style>
+ <div class="footer">
+          <div class="style1"> <p> All Rights Reserved. &copy; 2021  Designed By : Ndiene Justine Uko, N/2019/CSE/022 , Foundation Polytecnic, Ikot Idem,Ikot Ekpene  </p>
+</div>
+</div>
